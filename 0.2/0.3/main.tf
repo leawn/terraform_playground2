898309ef113bb2bf52352884d0211b5c7f037e6f
Leon <leonrubner2809@gmail.com>
2021-09-06 12:45:29 +0200

    initial commit

---
+++ 0.2/0.3/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-smth"
+  instance_type = "t2.micro"
+  tags = {
+      Name = "terraform-example"
+  }
+}
