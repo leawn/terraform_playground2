87fa7f15c1dc473c0279ec105fab9eed61a29acf
Leon <leonrubner2809@gmail.com>
2021-09-06 14:07:00 +0200

    deploying a cluster of web servers with elb(alb) load balancer

--- 0.2/0.4/main.tf
+++ 0.2/0.4/main.tf
@@ -2,6 +2,12 @@ provider "aws" {
   region = "us-east-2"
 }
 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type = number
+  default = 8080
+}
+
 resource "aws_instance" "example" {
   ami = "ami-smth"
   instance_type = "t2.micro"
@@ -10,9 +16,9 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
   #!/bin/bash
   echo "Yup" > index.html
-  nohup busybox httpd -f -p 8080 &
+  nohup busybox httpd -f -p ${var.server_port} &
   EOF
-  
+
   tags = {
       Name = "terraform-example"
   }
@@ -22,9 +28,14 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-      from_port = 8080
-      to_port = 8080
+      from_port = var.server_port
+      to_port = var.server_port
       protocol = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
 }
