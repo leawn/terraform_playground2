b52fedbf887cc907f51aa553fd1912f6974a7129
Leon <leonrubner2809@gmail.com>
2021-09-06 20:28:39 +0200

    reusable modules terraform commit

---
+++ 0.3/0.2/live/stage/services/webserver-cluster/main.tf
@@ -0,0 +1,51 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+module "webserver_cluster" {
+  source = "../../../modules/services/webserver-cluster"
+  # source = "github.com/foo/modules//webserver-cluster?ref=v0.0.1"
+  
+  cluster_name = "webserver-stage"
+  db_remote_state_bucket = "SOME_NAME"
+  db_remote_state_key = "stage/data-stores/mysql/terraform.tfstate"
+
+  instance_type = "t2.micro"
+  min_size = 2
+  max_size = 2
+}
+
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+  scheduled_action_name = "scale-out-during-business-hours"
+  min_size = 2
+  max_size = 10
+  desired_capacity = 10
+  recurrence = "0 9 * * *"
+
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  scheduled_action_name = "scale-in-at-night"
+  min_size = 2
+  max_size = 10
+  desired_capacity = 2
+  recurrence = "0 17 * * *"
+
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
+
+resource "aws_security_group_rule" "allow_testing_inbound" {
+  type = "ingress"
+  security_group_id = module.webserver_cluster.alb_security_group_id
+
+  from_port = 12345
+  to_port = 12345
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+output "alb_dns_name" {
+  value = module.webserver_cluster.alb_dns_name
+  description = "The domain name of the load balancer"
+}
--- 0.3/0.2/modules/services/webserver-cluster/main.tf
+++ 0.3/0.2/modules/services/webserver-cluster/main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "us-east-2"
-}
-
 data "aws_vpc" "default" {
   default = true
 }
@@ -10,8 +6,18 @@ data "aws_subnet_ids" "default" {
   vpc_id = data.aws_vpc.default.id
 }
 
+data "terraform_remote_state" "db" {
+  backend = "s3"
+
+  config = {
+    bucket = var.db_remote_state_bucket
+    key = var.db_remote_state_key
+    region = "us-east-2"
+   }
+}
+
 data "template_file" "user_data" {
-  template = file("user-data.sh")
+  template = file("${path.module}/user-data.sh")
 
   vars = {
     server_port = var.server_port
@@ -20,8 +26,16 @@ data "template_file" "user_data" {
   }
 }
 
+locals {
+  http_port = 80
+  any_port = 0
+  any_protocol = "-1"
+  tcp_protocol = "tcp"
+  all_ips = ["0.0.0.0/0"]
+}
+
 resource "aws_lb" "example" {
-  name = "terraform-asg-example"
+  name = "${var.cluster_name}-aws_lb-example"
   load_balancer_type = "application"
   subnets = data.aws_subnet_ids.default.ids
   security_groups = [aws_security_group.alb.id]
@@ -29,7 +43,7 @@ resource "aws_lb" "example" {
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.example.arn
-  port = 80
+  port = local.http_port
   protocol = "HTTP"
 
   default_action {
@@ -44,7 +58,7 @@ resource "aws_lb_listener" "http" {
 }
 
 resource "aws_lb_target_group" "asg" {
-  name = "terraform-asg-example"
+  name = "${var.cluster_name}-asg"
   port = var.server_port
   protocol = "HTTP"
   vpc_id = data.aws_vpc.default.id
@@ -62,7 +76,7 @@ resource "aws_lb_target_group" "asg" {
 
 resource "aws_launch_configuration" "example" {
   image_id = "ami-smth"
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   security_groups = [aws_security_group.instance.id]
 
   user_data = data.template_file.user_data.rendered
@@ -79,12 +93,12 @@ resource "aws_autoscaling_group" "example" {
   target_group_arns = [aws_lb_target_group.asg.arn]
   health_check_type = "ELB"
 
-  min_size = 2
-  max_size = 10
+  min_size = var.min_size
+  max_size = var.max_size
 
   tag {
     key = "Name"
-    value = "terraform-asg-example"
+    value = "${var.cluster_name}"
     propagate_at_launch = true
   }
 }
@@ -105,25 +119,45 @@ resource "aws_lb_listener_rule" "asg" {
 }
 
 resource "aws_security_group" "alb" {
-  name = "terraform-example-alb"
+  name = "${var.cluster_name}-alb"
 
   ingress {
-      from_port = 80
-      to_port = 80
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+      from_port = local.http_port
+      to_port = local.http_port
+      protocol = local.tcp_protocol
+      cidr_blocks = local.all_ips
   }
 
   egress {
-      from_port = 0
-      to_port = 0
-      protocol = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
+      from_port = local.any_port
+      to_port = local.any_port
+      protocol = local.any_protocol
+      cidr_blocks = local.all_ips
   }
 }
 
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type = "ingress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port = local.http_port
+  to_port = local.http_port
+  protocol = local.tcp_protocol
+  cidr_blocks = local.all_ips
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type = "egress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port = local.any_port
+  to_port = local.any_port
+  protocol = local.any_protocol
+  cidr_blocks = local.all_ips
+}
+
 resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
+  name = "${var.cluster_name}-instance"
 
   ingress {
       from_port = var.server_port
